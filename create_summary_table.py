e0d4228a43afdbede7fa0b963509354f23a82775
jess-breda <jbreda19@gmail.com>
2023-05-30 08:29:38 -0400

    notebook 4 compelete

--- create_summary_table.py
+++ create_summary_table.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import datetime
 from datajoint.errors import DataJointError
+from pathlib import Path
+import os
 
 
 ratinfo = dj.create_virtual_module("intfo", "ratinfo")
@@ -18,8 +20,28 @@ def create_daily_summary_from_dj(
     animal_ids=None, date_min="2000-01-01", date_max="2030-01-01", verbose=False
 ):
     """
-    TODO- PRIMARY FUNCTION
-    TODO- add in overwrite/save out functions due to slow load in
+    Function to create a daily summary table from DataJoint
+    tables. This function is a wrapper for the following
+    functions:
+        - create_animal_daily_summary_df
+
+    params
+    ------
+    animal_ids : list (optional, default = None)
+        list of animal ids to query. If None, defaults to ANIMAL_IDS
+        at the top of this script
+    date_min : str (optional, default = "2000-01-01")
+        minimum date to query in YYYY-MM-DD format, e.g. "2022-01-04"
+    date_max : str (optional, default = "2030-01-01")
+        maximum date to query in YYYY-MM-DD format, e.g. "2022-01-04"
+    verbose : bool (optional, default = False)
+        whether to print out verbose statements
+
+    returns
+    -------
+    daily_summary_df : pd.DataFrame
+        data frame containing daily summary info for all animals
+        in `animal_ids` between `date_min` and `date_max`
     """
     animal_ids = ANIMAL_IDS if animal_ids is None else animal_ids
     assert type(animal_ids) == list, "animal ids must be in a list"
@@ -27,7 +49,8 @@ def create_daily_summary_from_dj(
     animals_daily_summary_df = []
 
     for animal_id in animal_ids:
-        # TODO function for fetching valid dates
+        # create keys for querying dj tables to determine which dates
+        # to fetch
         subject_key = {"ratname": animal_id}
         sess_date_min_key = f"sessiondate >= '{date_min}'"
         sess_date_max_key = f"sessiondate <= '{date_max}'"
@@ -57,7 +80,27 @@ def create_daily_summary_from_dj(
 
 def create_animal_daily_summary_df(animal_id, dates, verbose=False):
     """
-    TODO
+    Function to create a daily summary table from DataJoint
+    tables for a given animal and dates. This function is a wrapper
+    for the following functions:
+        - fetch_daily_session_info
+        - fetch_daily_water_and_mass_info
+
+    params
+    ------
+    animal_id : str,
+        animal name e.g. "R501"
+    dates : list
+        list of dates to query in YYYY-MM-DD format, e.g. "2022-01-04"
+        that an animal had entry in Session or Mass table
+    verbose : bool (optional, default = False)
+        whether to print out verbose statements
+
+    returns
+    -------
+    daily_summary_df : pd.DataFrame
+        data frame containing daily summary info for a given animal
+        and dates
     """
     session_dfs = []
     water_mass_dfs = []
@@ -74,7 +117,8 @@ def create_animal_daily_summary_df(animal_id, dates, verbose=False):
     )
     if verbose:
         print(
-            f"fetched {len(dates)} daily summaries for {animal_id} between {min(dates)} and {max(dates)}"
+            f"\nfetched {len(dates)} daily summaries for {animal_id} "
+            f"from dj between {min(dates)} and {max(dates)}"
         )
 
     return daily_summary_df
@@ -87,7 +131,21 @@ def create_animal_daily_summary_df(animal_id, dates, verbose=False):
 
 def fetch_daily_session_info(animal_id, date):
     """
-    TODO
+    Function to generate a df row containing session info
+    for a given animal, date
+
+    params
+    ------
+    animal_id : str,
+        animal name e.g. "R501"
+    date : str
+        date to query in YYYY-MM-DD format, e.g. "2022-01-04"
+
+    returns
+    -------
+    D : pd.DataFrame
+        data frame row with summary session info for a
+        given animal, date
     """
 
     # fetch data
@@ -157,17 +215,17 @@ def fetch_daily_session_info(animal_id, date):
 
 def calculate_daily_train_dur(start_times, end_times, units="hours"):
     """
-    function that calculates total train time given
-    start and end times for a given animal, day
+    Function to calculate the amount of time an animal trained
+    for a given day
 
-    inputs
+    params
     ------
     start_times : arr
         array of start times as datetime.timedelta objects from sessions
-        table for a given animal, day. Typically of len == 1
+        table for a given animal, day. Typically of len == 1.
     end_times : arr
-        array of start times as datetime.timedelta objects from sessions
-        table for a given animal, day. Typically of len == 1
+        array of end times as datetime.timedelta objects from sessions
+        table for a given animal, day. Typically of len == 1.
     units : str, "hours" (default), "minutes" or "seconds
         what units to return trial duration
 
@@ -176,6 +234,7 @@ def calculate_daily_train_dur(start_times, end_times, units="hours"):
     daily_train_dur : float
         amount of time (in specified "units") a of training for given
         animal, day
+
     """
 
     if units == "hours":
@@ -202,9 +261,11 @@ def calculate_perf_metrics(perf_metrics, n_done_trials):
     ------
     perf_metrics: list
         List of performance metrics from Sessions table to calculate
-        weighted average of.
+        weighted average of. Typically of len == 4. Order is:
+        total_correct, percent_violations, right_correct, left_correct
     n_done_trials: list
-        List of number of trials from Sessions table
+        N_done_trials from Sessions table. Must be the same length
+        as perf_metrics.
 
     returns
     -------
@@ -230,20 +291,22 @@ def calculate_perf_metrics(perf_metrics, n_done_trials):
 
 
 def fetch_daily_water_and_mass_info(animal_id, date, verbose=False):
-    """ "
-    Wrapper function to generate a df row containing mass,
+    """
+    Function to generate a df row containing mass,
     water and restriction data for a given animal, date
 
-    inputs
+    params
     ------
     animal_id : str,
         animal name e.g. "R501"
     date : str
         date to query in YYYY-MM-DD format, e.g. "2022-01-04"
+    verbose : bool (optional, default = False)
+        whether to print out verbose statements
 
     returns
     -------
-    D : df
+    D : pd.DataFrame
         data frame row with mass, restriction and water data
         for a given animal, date
     """
@@ -251,7 +314,7 @@ def fetch_daily_water_and_mass_info(animal_id, date, verbose=False):
 
     D["animal_id"] = animal_id
     D["date"] = date
-    D["mass"], D["tech"] = fetch_daily_mass(animal_id, date)
+    D["mass"], D["tech"] = fetch_daily_mass(animal_id, date, verbose=verbose)
     D["percent_target"] = fetch_daily_restriction_target(animal_id, date)
     D["pub_volume"] = fetch_pub_volume(animal_id, date)
     D["rig_volume"] = fetch_rig_volume(animal_id, date, verbose=verbose)
@@ -268,17 +331,18 @@ def fetch_daily_water_and_mass_info(animal_id, date, verbose=False):
 
 def fetch_daily_water_target(mass, percent_target, date, verbose=False):
     """
-    Function for getting an animals water volume target on
-    a specific date
+    Function to calculate the water restriction target
+    for a given animal, date
 
-    inputs
+    params
     ------
     mass : float
-        mass in grams for a given animal, date fetched the Mass table
-        using fetch_daily_mass()
+        mass in grams for a given animal, date fetched from
+        the Mass table using fetch_daily_mass()
     percent_target : float
-        water restriction target in terms of percentage of body weight
-        fetched from the Water table using fetch_daily_restriction_target()
+        water restriction target in terms of percentage of
+        body weight fetched from the Water table using
+        fetch_daily_restriction_target()
     date : str or datetime
         date queried in YYYY-MM-DD format, e.g. "2022-01-04"
 
@@ -298,16 +362,20 @@ def fetch_daily_water_target(mass, percent_target, date, verbose=False):
     return volume_target
 
 
-def fetch_daily_mass(animal_id, date):
+def fetch_daily_mass(animal_id, date, verbose=False):
     """
     Function for getting an animals mass on
     a specific date
-    inputs
+
+    params
     ------
     animal_id : str,
         animal name e.g. "R501"
     date : str
         date to query in YYYY-MM-DD format, e.g. "2022-01-04"
+    verbose : bool (optional, default = False)
+        whether to print out verbose statements
+
     returns
     ------
     mass : float
@@ -320,10 +388,11 @@ def fetch_daily_mass(animal_id, date):
     try:
         mass, tech = (ratinfo.Mass & Mass_keys).fetch1("mass", "tech")
     except DataJointError:
-        print(
-            f"mass data not found for {animal_id} on {date}, but animal trained",
-            f"using previous days mass",
-        )
+        if verbose:
+            print(
+                f"mass data not found for {animal_id} on {date}, but animal trained",
+                f"using previous days mass",
+            )
         prev_date = date - datetime.timedelta(days=1)
         Mass_keys = {"ratname": animal_id, "date": prev_date}
         mass = float((ratinfo.Mass & Mass_keys).fetch1("mass"))
@@ -335,16 +404,19 @@ def fetch_daily_restriction_target(animal_id, date):
     """
     Function for getting an animals water
     target for a specific date
-    inputs
+
+    params
     ------
     animal_id : str,
         animal name e.g. "R501"
     date : str
         date to query in YYYY-MM-DD format, e.g. "2022-01-04
+
     returns
     ------
     percent_target : float
         water restriction target in terms of percentage of body weight
+
     note
     ----
     You can also fetch this code from the registry, but it's not
@@ -364,11 +436,12 @@ def fetch_daily_restriction_target(animal_id, date):
     # can't do fetch1 with this because water table
     # sometimes has a 0 entry and actual entry so
     # I'm taking the max to get around this
-    # this needs to be address w/ DJ people
     percent_target = (ratinfo.Water & Water_keys).fetch("percent_target")
 
     if len(percent_target) == 0:
-        percent_target = 4  # NOTE assumption made here to 4%- be careful!
+        # !!NOTE assumption made here will default to 3% for rats and 4% for mice
+        # !! in the fetch_daily_water_target() function
+        percent_target = 0
     elif len(percent_target) > 1:
         percent_target = percent_target.max()
 
@@ -385,6 +458,8 @@ def fetch_rig_volume(animal_id, date, verbose=False):
         animal name e.g. "R501"
     date : str
         date to query in YYYY-MM-DD format, e.g. "2022-01-04"
+    verbose : bool (optional, default = False)
+        whether to print out verbose statements
 
     returns
     -------
@@ -430,3 +505,120 @@ def fetch_pub_volume(animal_id, date):
         pub_volume = pub_volume.max()
 
     return float(pub_volume)
+
+
+######################
+### LAZY LOAD DATA ###
+######################
+
+
+def lazy_load_daily_summary_df(
+    animal_ids,
+    date_min,
+    date_max,
+    save_dir=os.getcwd(),
+    f_name="summary_table.csv",
+    save_out=False,
+    verbose=False,
+):
+    """
+    Function to load in a pre-saved daily summary table and append new dates if needed.
+    If no pre-saved df is found, then the function will fetch the data from DataJoint.
+
+    params
+    ------
+    animal_ids : list
+        list of animal ids to fetch data for, e.g. ["R610", "W600"]
+    date_min : str
+        start date in format "YYYY-MM-DD"
+    date_max : str
+        end date in format "YYYY-MM-DD"
+    save_dir : str (optional, default = os.getcwd())
+        directory to look for the pre-saved df, also used for save out
+    f_name : str (optional, default = "summary_table.csv")
+        name of the pre-saved df, also used for save out
+    save_out : bool (optional, default = False)
+        if any new info was fetched from dj, whether to save out. if
+        appending occurred, this will overwrite the pre-saved df
+    verbose : bool (optional, default = False)
+        whether to print out verbose statements in dj fetch functions
+
+    returns
+    -------
+    pandas.DataFrame
+        daily summary table with specified animal ids in range [date_min, date_max]
+    """
+    full_path = Path(save_dir).joinpath(f_name)
+
+    if full_path.exists():
+        # load in pre-saved df
+        pre_saved_df = pd.read_csv(full_path)
+
+        # check that all animals are in the pre-saved df
+        excluded_animals = np.setdiff1d(animal_ids, pre_saved_df.animal_id.unique())
+        assert len(excluded_animals) == 0, (
+            f"Pre-saved df does not contain {excluded_animals}! "
+            "Overwrite functionality assumes all animals are in the DataFrame."
+        )
+
+        # check if there are any new dates to load
+        new_dates = pd.date_range(start=date_min, end=date_max).difference(
+            pd.date_range(start=pre_saved_df.date.min(), end=pre_saved_df.date.max())
+        )
+
+        # no new dates to load, append the previously saved df
+        if not len(new_dates):
+            print(
+                f"Loaded pre-saved df with entries between {date_min} and {date_max}."
+            )
+            return pre_saved_df.query("date >=@date_min and date <=@date_max")
+
+        # there are new dates to fetch, need to update or min and max dates
+        new_min = new_dates.min().strftime("%Y-%m-%d")  # min date not in pre-saved df
+        new_max = new_dates.max().strftime("%Y-%m-%d")  # max date not in pre-saved df
+
+        if new_max < date_max:
+            print(f"partial dj load with new date max {date_max} -> {new_max}")
+            pre_saved_df = pre_saved_df.query("date <=@date_max")
+            date_max = new_max
+        elif new_min > date_min:
+            print(f"partial dj load with new date min {date_min} -> {new_min}")
+            pre_saved_df = pre_saved_df.query("date >=@date_min")
+            date_min = new_min
+        else:
+            print(
+                f"The provided date window is larger than the whole DataFrame. "
+                f"Only one-sided lazy loading can be performed. "
+                f"\nReturning the pre-saved DataFrame. "
+                f"with dates between {pre_saved_df.date.min()} and {pre_saved_df.date.max()}."
+            )
+            return pre_saved_df
+    else:
+        print(f"No pre-saved df found, fetching from DataJoint.")
+
+    # load in new data
+    dj_df = create_daily_summary_from_dj(
+        animal_ids=animal_ids, date_min=date_min, date_max=date_max, verbose=verbose
+    )
+
+    # append & return if we have a pre-saved df
+    if "pre_saved_df" in locals() and isinstance(pre_saved_df, pd.DataFrame):
+        appended_df = pd.concat([pre_saved_df, dj_df], ignore_index=True)
+        appended_df["date"] = pd.to_datetime(appended_df["date"]).dt.date
+        print(
+            f"Returning appended df with entries between {appended_df.date.min()} and {appended_df.date.max()}"
+        )
+
+        if save_out:
+            appended_df.to_csv(full_path, index=False)
+            print(f"Saved out appended df to {full_path}")
+
+        return appended_df.sort_values(by=["animal_id", "date"]).reset_index(drop=True)
+
+    # otherwise return the dj_df
+    else:
+        if save_out:
+            dj_df.to_csv(full_path, index=False)
+            print(f"Saved out dj df to {full_path}")
+        print(f"Returning dj df with entries between {date_min} and {date_max}")
+        return dj_df
